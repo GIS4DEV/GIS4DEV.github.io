c06b378f1e362ce523e86caa5c60c8f80364de4e
josephholler <joseph@josephholler.com>
2021-09-23 13:03:23 -0400

    update sql

--- assets/more.sql
+++ assets/more.sql
@@ -1,45 +1,44 @@
 /* DELETE DUPLICATE GEOMETRIES */
 
-	delete from mytable 
-	where myidcolumn IN (
-	    select t1.myidcolumn 
-	    from mytable mt1, mytable mt2
-	    where st_equals(mt1.the_geom, mt2.the_geom)
-	    and mt1.myidcolumn < mt2.myidcolumn
+	DELETE FROM mytable
+	WHERE myidcolumn IN (
+	    SELECT t1.myidcolumn
+	    FROM mytable mt1, mytable mt2
+	    WHERE st_equals(mt1.the_geom, mt2.the_geom)
+	    AND mt1.myidcolumn < mt2.myidcolumn
 	)
 	
 /* FIND MINIMUM DISTANCE TO ANY FEATURE IN ANOTHER TABLE */
 
-select geom, wname,  min(std) as minstd
- from
-(select wardflood.*, unipoint.name as uniname, st_distance(wardflood.geom, unipoint.geom) as std
-from wardflood left outer join unipoint
-on st_dwithin(wardflood.geom, unipoint.geom, 10000)) as a
-group by wname, geom
+SELECT geom, wname,  min(std) AS minstd
+ FROM
+(SELECT wardflood.*, unipoint.name AS uniname, st_distance(wardflood.geom, unipoint.geom) AS std
+FROM wardflood LEFT OUTER JOIN unipoint
+ON st_dwithin(wardflood.geom, unipoint.geom, 10000)) AS a
+GROUP BY wname, geom
 
 
 /* add a unique integer ID to a table */
-
 ALTER TABLE <table name> ADD COLUMN id SERIAL PRIMARY KEY;
 
 /* count points in polygons */
-SELECT id, count( field from points )
+SELECT id, count( <points field without nulls> )
 FROM points INNER JOIN polygons
 ON st_within(points.geom, polygons.geom) ) AS a
 GROUP BY id;
 
 
-/* count points in polygons with multiple queries */ 
+/* count points in polygons with multiple queries */
 
 
 /* Suggestion for a GIS UNION operation on two layers
 from  https://gis.stackexchange.com/questions/302086/postgis-union-of-two-polygons-layers */
-select distinct a.geom1 from
-(select distinct(st_dump(st_collect(t1.geom,t2.geom))).geom as geom1
- from t1 inner join t2 on not st_intersects(t1.geom,t2.geom)) a inner join 
-(select distinct (st_dump(st_collect(st_symdifference(t1.geom,t2.geom),st_intersection(t1.geom,t2.geom)))).geom
-from t1 ,t2 where st_intersects(t1.geom,t2.geom)) b
-on not st_intersects(a.geom1,b.geom)
-union
-select (st_dump(st_collect(st_symdifference(t1.geom,t2.geom),st_intersection(t1.geom,t2.geom)))).geom
-from t1 inner join t2 on st_intersects(t1.geom,t2.geom)
+SELECT DISTINCT a.geom1 FROM
+(SELECT distinct(st_dump(st_collect(t1.geom,t2.geom))).geom AS geom1
+ FROM t1 INNER JOIN t2 ON NOT st_intersects(t1.geom,t2.geom)) a INNER JOIN
+(SELECT DISTINCT (st_dump(st_collect(st_symdifference(t1.geom,t2.geom),st_intersection(t1.geom,t2.geom)))).geom
+FROM t1 ,t2 WHERE st_intersects(t1.geom,t2.geom)) b
+ON NOT st_intersects(a.geom1,b.geom)
+UNION
+SELECT (st_dump(st_collect(st_symdifference(t1.geom,t2.geom),st_intersection(t1.geom,t2.geom)))).geom
+FROM t1 INNER JOIN t2 ON st_intersects(t1.geom,t2.geom)
--- assets/osm_sql.sql
+++ assets/osm_sql.sql
@@ -1,5 +1,5 @@
 /*
-Structure Query Language Instructions for Dar es Salaam Flood Risk Analysis 
+Structure Query Language Instructions for Dar es Salaam Flood Risk Analysis
 Prepared by Joseph Holler, 2021
 Resources:
 	Panopto: https://midd.hosted.panopto.com/Panopto/Pages/Sessions/List.aspx?folderID=beda027d-3b8e-4700-9ae7-acf4012bdc0e
@@ -12,7 +12,7 @@ Resources:
 Nadine Schuurman argued for doing ethnographies on databases in order to understand data's provenance and social context, and to avoid making mistakes because of semantic heterogeneity.
 OpenStreetMap's model of volunteered geographic information provides a unique opportunity to gain insight into who has been editing data, when, and how frequently. You can even query historical versions of features on the map.
 
-In this tutorial, 
+In this tutorial,
 1) Let's practice some SQL queries to ask questions about provenance and social context in the OpenStreetMap data for Dar es Salaam, supplementing what we can already learn from Resilience Academy and Ramani Huria.
 and then
 2) Let's practice some more spatial SQL querying to find all of the pubs near universities in Dar es Salaam.
@@ -95,22 +95,33 @@ FROM
 GROUP BY osm_user
 ORDER BY n DESC;
 
-/* Who is making the most edits to Dar es Salaam OSM data, and do you think that local knowledge figures prominently in this data? */ 
+/* Who is making the most edits to Dar es Salaam OSM data, and do you think that
+local knowledge figures prominently in this data? */
 
-/* Let's try to find all of the universities in Dar es Salaam, and count how many pubs are found within 800 meters
-Start by searching for unique values in the amenity field of the polygons and points tables. */
+/* Let's try to find all of the universities in Dar es Salaam, and count how
+many pubs are found within 800 meters. Start by searching for unique values in
+the amenity field of the polygons and points tables. */
 
 -- enter queries here. It's similar to counting how many features each OSM user edited.
 
-/* Then, create new tables named unipoly and unipoint in your own schema containing the osm_id, way, and name for each point university feature and each polygon university features, using the proper UTM Zone
-NOTE: by default, new tables are created in your own schema, and queries look for tables in your own schema first and in the public schema second. If you need to specify which schema to query or create a table in, add the schema name and a period before the table name. E.g. use dumb.unipoint to find the unipoint table in dumb schema or use public.planet_osm_line to find the planet_osm_line table in the public schema. */
+/* Then, create new tables named unipoly and unipoint in your own schema
+containing the osm_id, way, and name for each point university feature and each
+polygon university features, using the proper UTM Zone.
+By default, new tables are created in your own schema, and queries look for
+tables in your own schema first and in the public schema second. If you need to
+specify which schema to query or create a table in, add the schema name and a
+period before the table name. E.g. use dumb.unipoint to find the unipoint table
+in dumb schema or use public.planet_osm_line to find the planet_osm_line table
+in the public schema. */
 
 CREATE TABLE unipoint AS
 SELECT osm_id, st_transform(way,32737)::geometry(point,32737) as geom, name
 FROM planet_osm_point
 WHERE amenity ILIKE 'university';
 
--- Repeat for the polygons. The ILIKE text string comparison operator is simlar to =, but allows for differences in capitalization
+/* Repeat for the polygons.
+The ILIKE text string comparison operator is simlar to =,
+but allows for differences in capitalization */
 
 /* Now lets create buffers */
 CREATE TABLE unibuffer AS
@@ -127,27 +138,38 @@ FROM unibuffer;
 CREATE TABLE unibuffers AS
 SELECT (st_dump(geom)).geom::geometry(polygon,32737)
 FROM unibufferdissolve;
-/* ST_Dump seperates all the parts of a geometry into separate features. It's documented here: https://postgis.net/docs/ST_Dump.html 
-I'm using this following the documentation, and do not know exactly why there is an extra set of parenthesis around st_dump folled by ".geom" which is like using AS to rename the output. I presume this is because st_dump() returns a set of rows, rather than just one. */
+/* ST_Dump seperates all the parts of a geometry into separate features.
+It's documented here: https://postgis.net/docs/ST_Dump.html
+I'm using this following the documentation, and do not know exactly why there is
+an extra set of parenthesis around st_dump folled by ".geom" which is like using
+AS to rename the output. I presume this is because st_dump() returns a set of
+rows, rather than just one. */
 
-/* Now we have single-part buffers! We can select by location for any of the pubs intersecting our buffers, remembering to keep the coordinate refrence systems consistent in the comparison. Transform the smaller set of features (buffers) to match the larger set (planet_osm_polygon).
-Remember that 'DISTINCT' avoids duplicates in case a pub was close to two or more university features */
+/* Now we have single-part buffers! We can select by location for any of the
+pubs intersecting our buffers, remembering to keep the coordinate refrence
+systems consistent in the comparison. Transform the smaller set of features
+(buffers) to match the larger set (planet_osm_polygon).
+Remember that 'DISTINCT' avoids duplicates in case a pub was close to two or
+more university features */
 
 SELECT DISTINCT osm_id, name
-FROM planet_osm_polygon INNER JOIN unibuffers 
+FROM planet_osm_polygon INNER JOIN unibuffers
 ON st_intersects(planet_osm_polygon.way, st_transform(unibuffers.geom,4326))
 WHERE planet_osm_polygon.amenity = 'pub';
 
-/* SQL is smart: it will solve the amenity = pub part of this first. What if we crafted this to require comparing all the spatial information first? The following query took 46 seconds instead of the 2.7 seconds for the query above  */
-
-SELECT DISTINCT osm_id, name 
-FROM planet_osm_polygon INNER JOIN unibuffers 
-ON st_intersects(planet_osm_polygon.way, st_transform(unibuffers.geom,4326));
-
-/* meanwhile, the following query was taking so long that I just cancelled it... why do you think the query below is so much more inefficient than the query above? */
+/* SQL is smart: it will solve the amenity = pub part of this first.
+What if we crafted this to require comparing all the spatial information first?
+The following query took 46 seconds instead of the 2.7 seconds for the query above */
 
 SELECT DISTINCT osm_id, name
-FROM planet_osm_polygon INNER JOIN unibufferdissolve 
+FROM planet_osm_polygon INNER JOIN unibuffers
+ON st_intersects(planet_osm_polygon.way, st_transform(unibuffers.geom,4326));
+
+/* meanwhile, the following query was taking so long that I just cancelled it...
+Why do you think the query below is so much more inefficient than the query above? */
+
+SELECT DISTINCT osm_id, name
+FROM planet_osm_polygon INNER JOIN unibufferdissolve
 ON st_intersects(planet_osm_polygon.way, st_transform(unibufferdissolve.geom,4326));
 
 /* Check to see if any pubs within 800 meters of a university are also hiding as point features! */
@@ -161,7 +183,5 @@ ON st_intersects(planet_osm_polygon.way, st_transform(unibufferdissolve.geom,432
 
 -- PostGIS Reference https://postgis.net/docs/reference.html
 -- cheat sheet: http://www.postgis.us/downloads/postgis21_cheatsheet.pdf
--- PostgreSQL cheat sheet: https://www.postgresqltutorial.com/postgresql-cheat-sheet/ 
--- nearest neighbor searching: https://postgis.net/workshops/postgis-intro/knn.html 
-
-
+-- PostgreSQL cheat sheet: https://www.postgresqltutorial.com/postgresql-cheat-sheet/
+-- nearest neighbor searching: https://postgis.net/workshops/postgis-intro/knn.html
